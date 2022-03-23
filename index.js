7abfefc463846621a1111408ade8c8b8dfb96426
Nithin3008 <60174062+Nithin3008@users.noreply.github.com>
2022-03-23 13:20:38 +0530

    Inital commit

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 var readlineSync = require('readline-sync');
 var score=0;
 var userName = readlineSync.question("What is your name? ");
-var welcomeMsg = "Welcome to the quiz " + userName + "! Let's see how well you know me!";
-console.log(welcomeMsg);
+var welcomeMsg = "Welcome to the quiz " + userName + "! Let's see how will you answer";
+
 var highScore=[
   {
-    name:"nikhil",
+    name:"aditya",
     score:5,
   },
   {
-    name:"srinevas",
+    name:"chandan",
     score:3,
   }
 ]
-function quiz(questions,answer)
+function quiz1(questions,answer)
 {
     var userAns = readlineSync.question(questions);
     if(answer.toUpperCase()===userAns.toUpperCase())
@@ -34,29 +34,29 @@ function quiz(questions,answer)
 
 var questionList=[
 {
-        question:"where do i live? ",
-        answer:"kurnool"
+        question:"what is tony stark best suit?  ",
+        answer:"marklxxxv"
       },
       {
-        question:"what is my favourite car? ",
-        answer:"rangerover"
+        question:"which company car is mostly used by tony stark ",
+        answer:"audi"
       },
       {
-        question:"who is my favourite superhero? ",
-        answer:"ironman"
+        question:"what is tony stark if not Irnoman ",
+        answer:"scientist"
       },
       {
-        question:"what is my favourite games genre? ",
-        answer:"racing"
+        question:"which year iron man 1 is released? ",
+        answer:"2008"
       },
       {
-        question: "What is my favourite colour? ",
-        answer: "orange"
+        question: "What is the name of tony stark daughter? ",
+        answer: "morgan stark"
       }
     ];
 for(var i=0;i<questionList.length;i++)
       {
-        quiz(questionList[i].question,questionList[i].answer)
+        quiz1(questionList[i].question,questionList[i].answer)
       }
 console.log("total score is ",score);
 for(var j=0;j<highScore.length;j++)
