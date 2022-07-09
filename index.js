6bc7da795312afbaab02ae6d6ab058d48fa9beec
Nithin <60174062+Nithin3008@users.noreply.github.com>
2022-07-09 10:49:04 +0530

    added new questions

--- index.js
+++ index.js
@@ -1,65 +1,68 @@
 var readlineSync = require('readline-sync');
-var score=0;
+var score = 0;
 var userName = readlineSync.question("What is your name? ");
 var welcomeMsg = "Welcome to the quiz " + userName + "! Let's see how will you answer";
 
-var highScore=[
+var highScore = [
   {
-    name:"aditya",
-    score:5,
+    name: "aditya",
+    score: 5,
   },
   {
-    name:"chandan",
-    score:3,
+    name: "chandan",
+    score: 3,
   }
 ]
-function quiz1(questions,answer)
-{
-    var userAns = readlineSync.question(questions);
-    if(answer.toUpperCase()===userAns.toUpperCase())
-    {
-      console.log("correct answer");
-      score++;
-      console.log(score);
-    }
-    else
-    {
-      console.log("wrong answer");
-      score--;
-      console.log(score);
-      
-    }
-  
+function quiz1(questions, answer) {
+  var userAns = readlineSync.question(questions);
+  if (answer.toUpperCase() === userAns.toUpperCase()) {
+    console.log("correct answer");
+    score++;
+    console.log(score);
+  }
+  else {
+    console.log("wrong answer");
+    score--;
+    console.log(score);
+
+  }
+
 }
 
-var questionList=[
-{
-        question:"what is tony stark best suit?  ",
-        answer:"marklxxxv"
-      },
-      {
-        question:"which company car is mostly used by tony stark ",
-        answer:"audi"
-      },
-      {
-        question:"what is tony stark if not Irnoman ",
-        answer:"scientist"
-      },
-      {
-        question:"which year iron man 1 is released? ",
-        answer:"2008"
-      },
-      {
-        question: "What is the name of tony stark daughter? ",
-        answer: "morgan stark"
-      }
-    ];
-for(var i=0;i<questionList.length;i++)
-      {
-        quiz1(questionList[i].question,questionList[i].answer)
-      }
-console.log("total score is ",score);
-for(var j=0;j<highScore.length;j++)
+var questionList = [
   {
-    console.log(highScore[j].name+ ":" + highScore[j].score);
+    question: "what is tony stark best suit?  ",
+    answer: "marklxxxv"
+  },
+  {
+    question: "which company car is mostly used by tony stark ",
+    answer: "audi"
+  },
+  {
+    question: "what is tony stark if not Irnoman ",
+    answer: "scientist"
+  },
+  {
+    question: "which year iron man 1 is released? ",
+    answer: "2008"
+  },
+  {
+    question: "What is the name of tony stark daughter? ",
+    answer: "morgan stark"
+  },
+  {
+    question: "What is the name of Ironman 1st suit? ",
+    answer: "mark one"
+  },
+  {
+    question: " What is the name of the suit he used to defeat hulk",
+    answer: "hulk buster suit "
   }
+];
+for (var i = 0; i < questionList.length; i++) {
+  quiz1(questionList[i].question, questionList[i].answer)
+}
+console.log("total score is ", score);
+for (var j = 0; j < highScore.length; j++) {
+  console.log(highScore[j].name + ":" + highScore[j].score);
+}
